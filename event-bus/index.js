5d2a248edffc6cff466d234ad7c82693db5a2127
teguhkhg <teguhkhg@gmail.com>
2023-05-02 14:49:22 +0700

    added query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,11 +4,21 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 app.post("/events", async (req, res) => {
   const data = req.body;
 
   await axios.post("http://localhost:4001/events", data);
   await axios.post("http://localhost:4002/events", data);
+  await axios.post("http://localhost:4003/events", data);
 
   res.status(200).send("ok");
 });
